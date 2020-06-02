b3a784f24ec33876ab1acf6269029aae6361e515
Naveen <naveenrohilla99@gmail.com>
2020-06-02 17:06:48 +0530

    trying

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
+  pwa: {
+    name: 'RNaveen99',
+    theme_color: '#1a1c1b',
+    iconPaths: [
+      {
+        src: './img/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: './img/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    start_url: '.',
+    display: 'standalone',
+    background_color: 'black',
+  },
 }
